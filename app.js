971191fda5a399ada5128e7193f01b5e67865b69
Esteban <esteban.chavez@tecsup.edu.pe>
2025-04-09 17:11:31 -0500

    Cambio de puerto a 9000

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
